3b298c354f3f06f393548b85ae4dc72348b4ebee
Rafael Pereira <rafaelspereira1@hotmail.com>
2023-02-24 16:46:41 -0300

    chore: add extra empty line

--- src/util.ts
+++ src/util.ts
@@ -2,6 +2,7 @@ export default class Util {
   static upperCaseFirstLetter(str: string): string {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
+
   static lowerCaseFirstLetter(str: string): string {
     return str.charAt(0).toLowerCase() + str.slice(1);
   }
