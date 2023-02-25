7960130fafbc1d66db36e7505be6b135822df71f
Rafael Pereira <rafaelspereira1@hotmail.com>
2023-02-24 23:28:30 -0300

    feat: create files module

---
+++ src/createFiles.ts
@@ -0,0 +1,69 @@
+import fsPromises from "fs/promises";
+import templates from "templates";
+import Util from "./util";
+import { ICreateFilesProps, IDependencies } from "./@types/createLayers.types";
+
+const defaultDepedencies = (
+  layer: keyof IDependencies,
+  componentName: string
+): string[] => {
+  const dependencies: IDependencies = {
+    repository: [],
+    service: [`${componentName}Repository`],
+    factory: [`${componentName}Repository`, `${componentName}Service`],
+  };
+  return dependencies[layer].map(Util.lowerCaseFirstLetter);
+};
+async function executeWrites(
+  PendingFilesToWrite: { fileNamePath: string; txtFile: string }[]
+): Promise<void> {
+  await Promise.all(
+    PendingFilesToWrite.map(({ fileNamePath, txtFile }) =>
+      fsPromises.writeFile(fileNamePath, txtFile)
+    )
+  );
+}
+
+export default async function createFiles({
+  mainPath,
+  defaultMainFolder,
+  layers,
+  componentName,
+}: ICreateFilesProps) {
+  const keys = Object.keys(templates);
+
+  const PendingFilesToWrite: {
+    fileNamePath: string;
+    txtFile: string;
+  }[] = [];
+
+  layers.forEach((layer) => {
+    const chosenTemplate = keys.find((key) => key.includes(layer));
+    if (!chosenTemplate) {
+      throw new Error(`Template not found for layer "${layer}"`);
+    }
+
+    // @ts-ignore
+    const template = templates[chosenTemplate];
+
+    // exemple: /users/documents/username/codegen/src/factory
+    const targetFolder = `${mainPath}/${defaultMainFolder}/${layer}`;
+    // @ts-ignore
+    const depedencies = defaultDepedencies(layer, componentName);
+
+    const { fileName, template: txtFile } = template(
+      componentName,
+      ...depedencies
+    );
+
+    const fileNamePath = `${targetFolder}/${Util.lowerCaseFirstLetter(
+      fileName
+    )}.ts`;
+
+    PendingFilesToWrite.push({ fileNamePath, txtFile });
+  });
+
+  await executeWrites(PendingFilesToWrite);
+
+  return { sucess: true };
+}
---
+++ test/unit/files.test.ts
@@ -0,0 +1,112 @@
+import templates from "templates";
+import { expect, describe, jest, beforeEach, it } from "@jest/globals";
+import createFiles from "createFiles";
+
+import fsPromises from "fs/promises";
+
+describe("#Files - Folder Structure", () => {
+  const defaultLayers = ["service", "factory", "repository"];
+  const config = {
+    layers: defaultLayers,
+    mainPath: "./",
+    defaultMainFolder: "src",
+    componentName: "test",
+  };
+
+  const repositoryLayer = `${config.componentName}Repository`;
+  const serviceLayer = `${config.componentName}Service`;
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.clearAllMocks();
+  });
+
+  it("should not create a file structure if the template do not exist", async () => {
+    const myConfig = {
+      ...config,
+      layers: ["inexistent"],
+    };
+
+    const expectedErrorMessage = `Template not found for layer "inexistent"`;
+
+    try {
+      await createFiles(myConfig);
+    } catch (error) {
+      expect(error.message).toEqual(expectedErrorMessage);
+    }
+  });
+  it("repository should not have any additional dependencies", async () => {
+    jest
+      .spyOn(fsPromises, "writeFile")
+      .mockImplementation(() => Promise.resolve());
+    jest.spyOn(templates, "repositoryTemplate").mockReturnValue({
+      fileName: "",
+      template: "",
+    });
+
+    const myConfig = {
+      ...config,
+      layers: ["repository"],
+    };
+
+    const expected = { sucess: true };
+
+    const result = await createFiles(myConfig);
+
+    expect(result).toEqual(expected);
+    expect(fsPromises.writeFile).toHaveBeenCalledTimes(myConfig.layers.length);
+    expect(templates.repositoryTemplate).toHaveBeenCalledWith(
+      myConfig.componentName
+    );
+  });
+  it("service should have repository as a depedency", async () => {
+    jest
+      .spyOn(fsPromises, "writeFile")
+      .mockImplementation(() => Promise.resolve());
+    jest.spyOn(templates, "serviceTemplate").mockReturnValue({
+      fileName: "",
+      template: "",
+    });
+
+    const myConfig = {
+      ...config,
+      layers: ["repository", "service"],
+    };
+
+    const expected = { sucess: true };
+
+    const result = await createFiles(myConfig);
+
+    expect(result).toEqual(expected);
+    expect(fsPromises.writeFile).toHaveBeenCalledTimes(myConfig.layers.length);
+    expect(templates.serviceTemplate).toHaveBeenCalledWith(
+      myConfig.componentName,
+      repositoryLayer
+    );
+  });
+  it("factory should have repository and service as dependencies", async () => {
+    jest
+      .spyOn(fsPromises, "writeFile")
+      .mockImplementation(() => Promise.resolve());
+    jest.spyOn(templates, "factoryTemplate").mockReturnValue({
+      fileName: "",
+      template: "",
+    });
+
+    const myConfig = {
+      ...config,
+    };
+
+    const expected = { sucess: true };
+
+    const result = await createFiles(myConfig);
+
+    expect(result).toEqual(expected);
+    expect(fsPromises.writeFile).toHaveBeenCalledTimes(myConfig.layers.length);
+    expect(templates.factoryTemplate).toHaveBeenCalledWith(
+      myConfig.componentName,
+      repositoryLayer,
+      serviceLayer
+    );
+  });
+});
