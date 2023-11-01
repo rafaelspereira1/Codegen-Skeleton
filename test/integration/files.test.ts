cd9f63968836a54655b23ff3557767cb65b4aba7
Rafael Pereira <rafaelspereira1@hotmail.com>
2023-11-01 11:18:32 -0300

    feat: files, folders and layers integration tests

--- test/integration/files.test.ts
+++ test/integration/files.test.ts
@@ -11,19 +11,27 @@ import {
 import { tmpdir } from "os";
 import fsPromises from "fs/promises";
 import { join } from "path";
-import createLayersIfNotExists from "createLayers";
 import createFiles from "createFiles";
+import createLayersIfNotExists from "createLayers";
+import { ICreateFilesProps } from "../../src/@types/createLayers.types";
+import Util from "../../src/util";
 
-async function getFolders({
+function getAllFunctionsFromInstance(instance: unknown) {
+  return Reflect.ownKeys(Object.getPrototypeOf(instance)).filter(
+    (method) => method !== "constructor"
+  );
+}
+function generateFilePath({
   mainPath,
   defaultMainFolder,
-}: {
-  mainPath: string;
-  defaultMainFolder: string;
-}): Promise<string[]> {
-  return fsPromises.readdir(join(mainPath, defaultMainFolder));
+  layers,
+  componentName,
+}: ICreateFilesProps) {
+  return layers.map((layer) => {
+    const filename = `${componentName}${Util.upperCaseFirstLetter(layer)}.ts`;
+    return join(mainPath, defaultMainFolder, layer, filename);
+  });
 }
-
 describe("#Integration - Files - Files Structure", () => {
   const config = {
     defaultMainFolder: "src",
@@ -33,7 +41,7 @@ describe("#Integration - Files - Files Structure", () => {
   };
 
   const packageJson = "package.json";
-  const packageJsonLocation = join("./test/integration/mocks");
+  const packageJsonLocation = join("./test/integration/mocks", packageJson);
 
   beforeAll(async () => {
     config.mainPath = await fsPromises.mkdtemp(join(tmpdir(), "files-"));
@@ -41,11 +49,13 @@ describe("#Integration - Files - Files Structure", () => {
       packageJsonLocation,
       join(config.mainPath, packageJson)
     );
+
     await createLayersIfNotExists(config);
   });
 
   afterAll(async () => {
-    await fsPromises.rm(config.mainPath, { recursive: true });
+    console.log(config.mainPath);
+    // await fsPromises.rm(config.mainPath, { recursive: true });
   });
 
   beforeEach(() => {
@@ -60,7 +70,61 @@ describe("#Integration - Files - Files Structure", () => {
     };
 
     await createFiles(myConfig);
+
+    const [repositoryFile] = generateFilePath(myConfig);
+
+    const { default: Repository } = await import(repositoryFile);
+
+    const instance = new Repository();
+
+    const expectNotImplemented = (fn: Function) =>
+      expect(() => fn.call(instance)).rejects.toEqual("Not implemented");
+
+    expectNotImplemented(instance.create);
+    expectNotImplemented(instance.read);
+    expectNotImplemented(instance.update);
+    expectNotImplemented(instance.delete);
   });
-  it("Service class should have create, read, update and delete methods and should call all Repository methods", async () => {});
-  it("Factory instance should match layers", async () => {});
+
+  it("Service class should have create, read, update and delete methods and should call all Repository methods", async () => {
+    const myConfig = {
+      ...config,
+      layers: ["repository", "service"],
+    };
+
+    try {
+      await createFiles(myConfig);
+
+      const [repositoryFile, serviceFile] = generateFilePath(myConfig);
+
+      const { default: Repository } = await import(repositoryFile);
+
+      const { default: Service } = await import(serviceFile);
+
+      const repository = new Repository();
+      const service = new Service({ repository });
+
+      const allRepositoryMethods = getAllFunctionsFromInstance(repository);
+
+      allRepositoryMethods.forEach((method) => {
+        if (typeof method === "symbol") {
+          // eslint-disable-next-line no-param-reassign
+          method = method.toString();
+        }
+        jest.spyOn(repository, method).mockResolvedValue(null);
+      });
+
+      getAllFunctionsFromInstance(service).forEach((method) =>
+        service[method].call(service, [])
+      );
+
+      allRepositoryMethods.forEach((method) =>
+        expect(repository[method]).toHaveBeenCalled()
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  // it("Factory instance should match layers", async () => {});
 });
